c77785c6301ca21ca21edffd53ca64705c6134f6
Michelle Chang <mchang98@uw.edu>
2017-10-19 15:00:33 -0700

    completed exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -10,16 +10,18 @@ devtools::install_github("hadley/fueleconomy")
 
 
 # You should have have access to the `vehicles` data.frame
-
+vehicles <- data.frame(vehicles)
+is.data.frame(vehicles)
+colnames(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997 <- vehicles[vehicles == 1997]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+length(unique(vehicles.1997) == 1)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel.20mph <- 
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -10,26 +10,30 @@ devtools::install_github("hadley/fueleconomy")
 # Require/library the fueleconomy package
 
 # Install (if you haven't already) and load the `dplyr`package
-
+install.packages('dplyr')
 
 # You should have have access to the `vehicles` data.frame
 
 
 # Create a data.frame of vehicles from 1997
-
+cars.1997 <- filter(vehicles, year == 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(cars.1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel <- filter(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
-
+MakeYearFilter <- function(my.make, my.year) {
+  filtered <- filter(vehicles, make == my.make, year == my.year) %>%
+              filter(hwy == max(hwy))
+              
+}
 
 # What was the most efficient honda model of 1995?
 
